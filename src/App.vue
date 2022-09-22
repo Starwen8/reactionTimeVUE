6f4ec20e654c82700cfc77c9e06fd96971d277ed
Mingxinwen <starwen.cn@gmail.com>
2022-09-22 12:24:08 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <BlockBox v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResult" :score="score" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import BlockBox from "./components/BlockBox.vue";
+import Result from "./components/Result.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { BlockBox, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 3000;
+      this.isPlaying = true;
+      console.log(this.delay);
+      this.showResult = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    },
   },
 };
 </script>
